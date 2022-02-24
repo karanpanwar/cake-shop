de58131faffb255360350064a2c072027bc84ced
karanp <karanp@acquire.io>
2022-02-24 17:53:50 +0530

    Updates

---
+++ precache-manifest.5cc951ad4c552ebbcd4bf83be9e68af3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8581eda991d332c35fbff9f9c079f994",
+    "url": "/cake-shop/index.html"
+  },
+  {
+    "revision": "954adaa57cd30ccb6b8b",
+    "url": "/cake-shop/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "074265885c8c685bcfc0",
+    "url": "/cake-shop/static/css/main.4b5f2571.chunk.css"
+  },
+  {
+    "revision": "954adaa57cd30ccb6b8b",
+    "url": "/cake-shop/static/js/2.74982ca7.chunk.js"
+  },
+  {
+    "revision": "55b7ce6a4cb531d7802ed5f8e1dd818f",
+    "url": "/cake-shop/static/js/2.74982ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074265885c8c685bcfc0",
+    "url": "/cake-shop/static/js/main.6b1eaee7.chunk.js"
+  },
+  {
+    "revision": "cac8f8f1d03f33703ad6",
+    "url": "/cake-shop/static/js/runtime-main.f2575b55.js"
+  },
+  {
+    "revision": "3e7f038b85daa739336e4a3476c687f2",
+    "url": "/cake-shop/static/media/DMSans-Regular.3e7f038b.ttf"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/cake-shop/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "f338afd6056720d546ccc00427608a32",
+    "url": "/cake-shop/static/media/choco.f338afd6.jpg"
+  },
+  {
+    "revision": "6c1eedb7bc7db7bc598ae3a371696982",
+    "url": "/cake-shop/static/media/location.6c1eedb7.svg"
+  },
+  {
+    "revision": "22a8c4582e249418cc48a87c30c68df1",
+    "url": "/cake-shop/static/media/party.22a8c458.jpg"
+  },
+  {
+    "revision": "21842ab25799233757301f2d6307f1ac",
+    "url": "/cake-shop/static/media/wedding.21842ab2.jpg"
+  }
+]);
